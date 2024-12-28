2e020cc6946c9bc6296f6c0afeec9c8369b725ae
Sanjay <sdas69101@gmail.com>
2024-12-28 13:54:26 +0530

    hardcode

--- src/Login/firebase.js
+++ src/Login/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth,GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBB-OlM2yt8fVwNfCqCClIg5acj24eyJv0",
+  authDomain: "whatsapp-clone-d9906.firebaseapp.com",
+  projectId: "whatsapp-clone-d9906",
+  storageBucket: "whatsapp-clone-d9906.firebasestorage.app",
+  messagingSenderId: "5512218204",
+  appId: "1:5512218204:web:932db198d3df3fbd8d4899",
+  measurementId: "G-2C2KW9MCNL"
 };
 
 const app = initializeApp(firebaseConfig);
